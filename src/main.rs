3eee31dfd922df014320c0b58996c949744b78e8
Toni Guli <toniguli16@gmail.com>
2022-05-08 10:22:24 +0200

    teloxide hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use teloxide::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+    log::info!("Starting throw dice bot...");
+
+    let bot = Bot::from_env().auto_send();
+
+    teloxide::repl(bot, |message: Message, bot: AutoSend<Bot>| async move {
+        bot.send_dice(message.chat.id).await?;
+        respond(())
+    })
+    .await;
 }
