524e2692dcca94643b916739ef11e9b3bba5cd34
Toni Guli <toniguli16@gmail.com>
2022-06-12 16:57:23 +0200

    implemented function read_url_from_qr()

--- src/qr_reader.rs
+++ src/qr_reader.rs
@@ -1,1 +1,17 @@
+use rqrr::{PreparedImage, DeQRError};
 
+pub fn read_url_from_qr(path: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let img = image::open(path)?.to_luma8();
+
+    let mut img = PreparedImage::prepare(img);
+
+    let grids = img.detect_grids();
+
+    match grids.len() {
+        1 => {
+            let (_, content) = grids[0].decode()?;
+            Ok(content)
+        },
+        _ => Err(Box::new(DeQRError::DataUnderflow))
+    }
+}
